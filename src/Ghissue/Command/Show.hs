ac82a34a0cbc3a7e01c2fb68360190db6280cbc9
oscar <oscar@twitter.com>
2015-12-16 09:52:19 -1000

    Add comments to show --verbose. Fixes #15

--- src/Ghissue/Command/Show.hs
+++ src/Ghissue/Command/Show.hs
@@ -9,6 +9,8 @@ import Ghissue
 import Github.Auth(GithubAuth(..))
 import Github.Data.Definitions(Issue
                               , IssueLabel
+                              , IssueComment
+                              , IssueComment(..)
                               , issueNumber
                               , issueTitle
                               , issueLabels
@@ -16,6 +18,7 @@ import Github.Data.Definitions(Issue
                               , GithubDate(fromGithubDate)
                               , githubOwnerLogin)
 import Github.Issues
+import Github.Issues.Comments(comments')
 import Options.Applicative
 {-
   ghissue show 1,3 --label foo
@@ -36,7 +39,9 @@ showParser = let
   issueP = argument issuesReadM (metavar "ISSUES" <> help "issues to show, e.g. 1-2,4-5,8")
   labelP = strOption (long "label" <> short 'l' <> metavar "LABEL" <> help "labels which must be attached")
   verbose = switch (long "verbose" <> short 'v' <> help "Verbose: show full body and all comments")
-  tabs = switch (long "tabs" <> short 't' <> help "Use tabs for columns. Useful with sort -t $'\t' -k3 | column -t -n $'\t'")
+  tabs = switch (long "tabs" <>
+                 short 't' <>
+                 help "Use tabs for columns. Useful with sort -t $'\t' -k3 | column -t -n $'\t'")
   in ShowArgs <$> (concat <$> (many issueP)) <*> (many labelP) <*> verbose <*> tabs
 
 ownerName mgho = maybe "" githubOwnerLogin mgho
@@ -64,14 +69,18 @@ evenColumnarize rows = let
 glue2 :: (a -> b -> c) -> (c -> d) -> (a -> b -> d)
 glue2 fn gn = \a b ->  gn (fn a b)
 
+toLocalString :: TimeZone -> GithubDate -> String
+toLocalString tz ghd = let
+  updated = fromGithubDate ghd
+  in show (utcToLocalTime tz updated)
+
 toLineSummary :: TimeZone -> Issue -> [String]
 toLineSummary tz issue = let
   num = show $ issueNumber issue
   title = issueTitle issue
   labs = intercalate "," (sort $ map labelName (issueLabels issue))
   commentCount = show (issueComments issue)
-  updated = fromGithubDate (issueUpdatedAt issue)
-  local = show (utcToLocalTime tz updated)
+  local = toLocalString tz (issueUpdatedAt issue)
   assignee = ownerName (issueAssignee issue)
   in [num, title, local, commentCount, assignee, labs]
 
@@ -87,16 +96,39 @@ verboseStr tz issue = let
   body = fromMaybe "" (issueBody issue)
   in intercalate "\n" [header, body, ""]
 
-printResults :: ShowArgs -> [Issue] -> IO ()
-printResults sargs issues = do
+commentsFor :: Config -> Issue -> IO [IssueComment]
+commentsFor conf issue = do
+  let auth = GithubOAuth (configAuth conf)
+  let org = configGithubOrg conf
+  let repo = configRepo conf
+  let issueN = issueNumber issue
+  result <- comments' (Just auth) org repo issueN
+  failOnLeft result return
+
+commentToStr :: TimeZone -> IssueComment -> String
+commentToStr tz comment = let
+  user = githubOwnerLogin (issueCommentUser comment)
+  date = toLocalString tz (issueCommentUpdatedAt comment)
+  body = issueCommentBody comment
+  in user ++ " at " ++ date ++ "\n" ++ body ++ "\n\n"
+
+issueWithComment :: Config -> TimeZone -> Issue -> IO String
+issueWithComment conf tz issue = do
+  let issueStr = verboseStr tz issue
+  comments <- commentsFor conf issue
+  let commentStrs = map (commentToStr tz) comments
+  return (intercalate "\n" (issueStr : commentStrs))
+
+printResults :: Config -> ShowArgs -> [Issue] -> IO ()
+printResults conf sargs issues = do
   let matching = filter (\ls -> listContains (issueNumber ls) (showArgsIssues sargs)) issues
   tz <- getCurrentTimeZone
   let column = if (showTabs sargs) then tabColumnarize else evenColumnarize
-  let verbFn = map (verboseStr tz)
-  let shortFn = column . map (toLineSummary tz)
-  -- Here is a function: [Issue] -> [String]
+  let verbFn = mapM (issueWithComment conf tz)
+  let shortFn = return . column . map (toLineSummary tz)
+  -- Here is a function: [Issue] -> IO [String]
   let showFn = if (showVerbose sargs) then verbFn else shortFn
-  let lineSummaries = showFn matching
+  lineSummaries <- showFn matching
   sequence_ (map putStrLn lineSummaries)
 
 showAction :: Config -> ShowArgs -> IO ()
@@ -106,5 +138,4 @@ showAction conf sargs = do
   let repo = configRepo conf
   let limit = Labels (showArgsLabel sargs)
   eitherErrIssues <- issuesForRepo' (Just auth) org repo [limit]
-  case eitherErrIssues of Left err -> fail (show err)
-                          Right issues -> printResults sargs issues
+  failOnLeft eitherErrIssues (printResults conf sargs)
